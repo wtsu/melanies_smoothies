0c674b16f9b6e763c90577aaf3ccff2cae0c8bca
William Tsu <30533780+wtsu@users.noreply.github.com>
2024-11-04 12:45:43 -0500

    Update streamlit_app.py
    adding a random api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+fruityvice_response = requests.get("https://rickandmortyapi.com/documentation/#get-a-single-character")
+st.text(fruityvice_response)
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
